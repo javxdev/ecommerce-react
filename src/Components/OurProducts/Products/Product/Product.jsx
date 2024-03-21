9ca7fc3ef292d1fa3dc90c9715c05767efe6d8af
javierspr <learn.javier@gmail.com>
2024-03-21 12:36:49 -0300

    update products, new products

--- src/Components/OurProducts/Products/Product/Product.jsx
+++ src/Components/OurProducts/Products/Product/Product.jsx
@@ -2,7 +2,7 @@ function Product({ urlImg, title, description, price }) {
   return (
     <div className="w-52 h-80 pb-2 shadow-lg flex flex-col justify-between">
         <div className="my-auto px-2">
-            <img className="bg-cover bg-center max-h-[224px] mx-auto bg-white" src={urlImg} alt={title} />
+            <img className="bg-cover bg-center max-h-[200px] mx-auto bg-white" src={urlImg} alt={title} />
         </div>
         <div className="flex flex-col py-1 px-2">
             <span className="mt-auto font-semibold">{title}</span>
--- src/Components/OurProducts/Products/Products.jsx
+++ src/Components/OurProducts/Products/Products.jsx
@@ -4,25 +4,36 @@ import Product from './Product/Product'
 function Products() {
     return (
     <div className='max-w-5xl mx-auto flex flex-wrap gap-y-20 gap-x-5 my-10'>
-        <Product urlImg="../../../src/assets/celular.jpg" title="Samsung A34 5G" description="Telefono celular Samsung" price={250000}/>
-        <Product urlImg="../../../src/assets/chaqueta.jpg" title="Chaqueta de Cuero" description="Telefono celular Samsung" price={50000}/>
-        <Product urlImg="../../../src/assets/anillos.jpg" title="Anillos Oro 24k" description="Telefono celular Samsung" price={70000}/>
-        <Product urlImg="../../../src/assets/chaqueta-mujer.jpg" title="Chaqueta invernal" description="Telefono celular Samsung" price={26500}/>
-        <Product urlImg="../../../src/assets/monitor.jpg" title="Monitor 144hz ViewSonic" description="Telefono celular Samsung" price={440000}/>
-        <Product urlImg="../../../src/assets/celular.jpg" title="Samsung A34 5G" description="Telefono celular Samsung" price={250000}/>
-        <Product urlImg="../../../src/assets/chaqueta.jpg" title="Chaqueta de Cuero" description="Telefono celular Samsung" price={50000}/>
-        <Product urlImg="../../../src/assets/anillos.jpg" title="Anillos Oro 24k" description="Telefono celular Samsung" price={70000}/>
-        <Product urlImg="../../../src/assets/chaqueta-mujer.jpg" title="Chaqueta invernal" description="Telefono celular Samsung" price={26500}/>
-        <Product urlImg="../../../src/assets/monitor.jpg" title="Monitor 144hz ViewSonic" description="Telefono celular Samsung" price={440000}/>
-        <Product urlImg="../../../src/assets/chaqueta.jpg" title="Chaqueta de Cuero" description="Telefono celular Samsung" price={50000}/>
-        <Product urlImg="../../../src/assets/anillos.jpg" title="Anillos Oro 24k" description="Telefono celular Samsung" price={70000}/>
-        <Product urlImg="../../../src/assets/chaqueta-mujer.jpg" title="Chaqueta invernal" description="Telefono celular Samsung" price={26500}/>
-        <Product urlImg="../../../src/assets/monitor.jpg" title="Monitor 144hz ViewSonic" description="Telefono celular Samsung" price={440000}/>
-        <Product urlImg="../../../src/assets/chaqueta.jpg" title="Chaqueta de Cuero" description="Telefono celular Samsung" price={50000}/>
-        <Product urlImg="../../../src/assets/anillos.jpg" title="Anillos Oro 24k" description="Telefono celular Samsung" price={70000}/>
-        <Product urlImg="../../../src/assets/chaqueta-mujer.jpg" title="Chaqueta invernal" description="Telefono celular Samsung" price={26500}/>
-        <Product urlImg="../../../src/assets/monitor.jpg" title="Monitor 144hz ViewSonic" description="Telefono celular Samsung" price={440000}/>
-        <Product urlImg="../../../src/assets/celular.jpg" title="Samsung A34 5G" description="Telefono celular Samsung" price={250000}/>
+        <Product urlImg="../../../src/assets/celular.jpg" title="Samsung A34 5G" description="Snapdragon 852 16gb 48mpx" price={250000}/>
+        <Product urlImg="../../../src/assets/chaqueta.jpg" title="Chaqueta de Cuero" description="EcoCuero reversible" price={50000}/>
+        <Product urlImg="../../../src/assets/anillos.jpg" title="Anillo Oro 24k" description="Anillo Oro 24k" price={70000}/>
+        <Product urlImg="../../../src/assets/chaqueta-mujer.jpg" title="Chaqueta invernal" description="Costura reforzada" price={26500}/>
+        <Product urlImg="../../../src/assets/monitor.jpg" title="Monitor 144hz ViewSonic" description="1m/s IPS antireflex" price={440000}/>
+        <Product urlImg="../../../src/assets/anillo-diamante.jpg" title="Anillo diamante" description="Diamantes incrustados" price={1000000}/>
+        <Product urlImg="../../../src/assets/conjunto-mujer.jpg" title="Conjunto de mujer" description="Zapatillas, Remera y Short" price={120000}/>
+        <Product urlImg="../../../src/assets/mochila.jpg" title="Mochila vintage" description="Tela resistente tipo vintage" price={23000}/>
+        <Product urlImg="../../../src/assets/disco-rigido.jpg" title="Disco Rigido" description="1TB HDD WD" price={63000}/>
+        <Product urlImg="../../../src/assets/remera-mujer.jpg" title="Remera M Blanca" description="Cuello en V talle M" price={11000}/>
+        <Product urlImg="../../../src/assets/celular.jpg" title="Samsung A34 5G" description="Snapdragon 852 16gb 48mpx" price={250000}/>
+        <Product urlImg="../../../src/assets/chaqueta.jpg" title="Chaqueta de Cuero" description="EcoCuero reversible" price={50000}/>
+        <Product urlImg="../../../src/assets/anillos.jpg" title="Anillo Oro 24k" description="Anillo Oro 24k" price={70000}/>
+        <Product urlImg="../../../src/assets/chaqueta-mujer.jpg" title="Chaqueta invernal" description="Costura reforzada" price={26500}/>
+        <Product urlImg="../../../src/assets/monitor.jpg" title="Monitor 144hz ViewSonic" description="1m/s IPS antireflex" price={440000}/>
+        <Product urlImg="../../../src/assets/anillo-diamante.jpg" title="Anillo diamante" description="Diamantes incrustados" price={1000000}/>
+        <Product urlImg="../../../src/assets/conjunto-mujer.jpg" title="Conjunto de mujer" description="Zapatillas, Remera y Short" price={120000}/>
+        <Product urlImg="../../../src/assets/mochila.jpg" title="Mochila vintage" description="Tela resistente tipo vintage" price={23000}/>
+        <Product urlImg="../../../src/assets/disco-rigido.jpg" title="Disco Rigido" description="1TB HDD WD" price={63000}/>
+        <Product urlImg="../../../src/assets/remera-mujer.jpg" title="Remera M Blanca" description="Cuello en V talle M" price={11000}/>
+        <Product urlImg="../../../src/assets/celular.jpg" title="Samsung A34 5G" description="Snapdragon 852 16gb 48mpx" price={250000}/>
+        <Product urlImg="../../../src/assets/chaqueta.jpg" title="Chaqueta de Cuero" description="EcoCuero reversible" price={50000}/>
+        <Product urlImg="../../../src/assets/anillos.jpg" title="Anillo Oro 24k" description="Anillo Oro 24k" price={70000}/>
+        <Product urlImg="../../../src/assets/chaqueta-mujer.jpg" title="Chaqueta invernal" description="Costura reforzada" price={26500}/>
+        <Product urlImg="../../../src/assets/monitor.jpg" title="Monitor 144hz ViewSonic" description="1m/s IPS antireflex" price={440000}/>
+        <Product urlImg="../../../src/assets/anillo-diamante.jpg" title="Anillo diamante" description="Diamantes incrustados" price={1000000}/>
+        <Product urlImg="../../../src/assets/conjunto-mujer.jpg" title="Conjunto de mujer" description="Zapatillas, Remera y Short" price={120000}/>
+        <Product urlImg="../../../src/assets/mochila.jpg" title="Mochila vintage" description="Tela resistente tipo vintage" price={23000}/>
+        <Product urlImg="../../../src/assets/disco-rigido.jpg" title="Disco Rigido" description="1TB HDD WD" price={63000}/>
+        <Product urlImg="../../../src/assets/remera-mujer.jpg" title="Remera M Blanca" description="Cuello en V talle M" price={11000}/>
     </div>
   )
 }
